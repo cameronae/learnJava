80d04904f6b81aee59cf6fdcbac05d5df4736588
cameronae <cameronjreiff@yahoo.com>
2025-05-24 21:26:30 -0400

    initial commit

---
+++ introToJava.java
@@ -0,0 +1,14 @@
+//All java programs are inside of a class, have to name the class as the file name
+public class introToJava{
+    //this is like int main()
+    public static void main(String[] args) {
+        //primative data types are int, double, char, boolean
+        //reference data types are string, 
+        int age = 19;
+        System.out.print("Hello World\n");
+        System.out.println("This prints a line with auto new line");
+        System.out.println("I am " + age + " years old");
+        //sout tab creates a print
+        System.out.println();
+    }
+}
