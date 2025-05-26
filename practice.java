96905779664af25f5505fd7bc9ec158887b899da
cameronae <cameronjreiff@yahoo.com>
2025-05-25 23:15:27 -0400

    done for the night

---
+++ practice.java
@@ -0,0 +1,10 @@
+public class practice {
+    public static void main(String[] args) {
+        
+    String email = "cameron.reiff@gmail.com";
+
+    String username = email.substring(0, email.lastIndexOf("@"));
+    String kindofEmail = email.substring(email.lastIndexOf("@") + 1,email.lastIndexOf("."));
+    System.out.println(email + ' ' + username + ' ' + kindofEmail);
+    }
+}
